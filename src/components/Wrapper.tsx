0d18b8f94f644cd7877f874be6ad3e475f3148ae
Your New Name <vipinkumarti555@gmail.com>
2024-08-19 22:31:05 +0530

    Nothing

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,12 @@
+import { cn } from '@/lib/utils'
+import React, { ReactNode } from 'react'
+
+function Wrapper({className,children}:{className?:string ,children:ReactNode}) {
+  return (
+    <div className={cn(className,"mx-auto w-full max-w-xl px-2.5 md:px-20")}>
+            {children}
+    </div>
+  )
+}
+
+export default Wrapper
